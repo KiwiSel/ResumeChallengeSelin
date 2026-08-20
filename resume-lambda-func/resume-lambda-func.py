152da887d795256910307434ff533d0182d283b6
KiwiSel <S.A.Mehmed@student.tudelft.nl>
2026-08-20 19:49:32 +0300

    initial lambda func

---
+++ resume-lambda-func/resume-lambda-func.py
@@ -0,0 +1,27 @@
+import boto3
+
+dynamodb = boto3.resource("dynamodb")
+table = dynamodb.Table("ResumeTable")
+
+
+def lambda_handler(event, context):
+    response = table.update_item(
+        Key={
+            "id": "visitors"
+        },
+        UpdateExpression="ADD #count :increment",
+        ExpressionAttributeNames={
+            "#count": "count"
+        },
+        ExpressionAttributeValues={
+            ":increment": 1
+        },
+        ReturnValues="UPDATED_NEW"
+    )
+
+    count = response["Attributes"]["count"]
+
+    return {
+        "statusCode": 200,
+        "body": str(count)
+    }
